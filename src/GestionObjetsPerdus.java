3a91aaac26cb9a94a2794150bcb30baf87604368
basmaaitoubih <basma.aitoubih@gmail.com>
2023-04-20 11:36:33 -0400

    option 1 et 2

--- src/GestionObjetsPerdus.java
+++ src/GestionObjetsPerdus.java
@@ -3,6 +3,9 @@ public class GestionObjetsPerdus {
     public static final String ENTETE_CHOIX1 = "\n=========================\nCONSIGNER UN OBJET PERDU\n=========================\n";
     public static final String ENTETE_CHOIX2 = "\n=========================\nRENDRE UN OBJET RECLAME\n=========================\n";
     public static final String ENTETE_CHOIX3 = "\n=========================\nRECHERCHER PAR MOTS CLES\n=========================\n" ;
+    public static final String ENTETE_CHOIX4 = "\n=========================\nRECHERCHER PAR DATE(S)\n=========================\n" ;
+    public static final String ENTETE_CHOIX5 = "\n=========================\nRECHERCHER PAR CATEGORIE\n=========================\n" ;
+    public static final String ENTETE_CHOIX6 = "\n=========================\nAFFICHER TOUS LES OBJETS CONSIGNES\n=========================\n" ;
     public static final String MENU_CATEGORIES= "CATEGORIES :\n1. bijou\n2. vetement\n3. argent / portefeuille\n4. cle(s)\n5. autre\n";
     public static final String MENU = "----\nMENU\n----\n";
     public static final String CHOIX1= "1. Consigner un objet perdu";
@@ -13,7 +16,7 @@ public class GestionObjetsPerdus {
     public static final String CHOIX6= "6. Afficher tous les objets consignes";
     public static final String CHOIX7= "7. Quitter";
     public static final String MSG_SOL_REP = "Voulez-vous vraiment rendre cet objet perdu (oui / non) :" ;
-
+    public static final String MSG_SOL_RES= "Specifier une (d)ate ou une (p)eriode :";
     public static final String MSG_SOL_MOT = "Mot(s) cle(s) recherches :" ;
     public static final String MSG_SOL_OBJETPERDU = "Entrez le numero d'identification (id) de l'objet a rendre :";
     public static final String MSG_SOL ="Entrez votre choix : ";
@@ -34,10 +37,13 @@ public class GestionObjetsPerdus {
 
     public static final String MSG_ERR_REP = "ERREUR, vous devez entrer OUI ou NON! RECOMMENCEZ..";
     public static final String MSG_ERR_MOT = "ERREUR, la chaine doit contenir entre 2 et 50 caracteres! RECOMMENCEZ..." ;
+    public static final String MSG_ERR_RES = "";
     public static final int MIN_MOT = 2;
     public static final int MAX_MOT = 50;
     public static final int MIN_LOC = 5;
     public static final int MAX_LOC = 50;
+    public static final char CAR_MAX = 'd';
+    public static final char CAR_MIN = 'p';
     public static final String REP1 = "oui";
     public static final String REP2 = "non";
 
@@ -76,6 +82,7 @@ public class GestionObjetsPerdus {
         String localisation;
         int numeroObjetPerdu;
         String choixRep;
+        char rep;
 
         ObjetPerdu[] objetsPerdus = TP3Utils.recupererDonnees();
         System.out.println(ENTETE);
@@ -109,13 +116,15 @@ public class GestionObjetsPerdus {
 
                 chaineMotsCles = TP3Utils.validerLngChaine(MSG_SOL_CLE, MSG_ERR_CLE, MIN_MOT, MAX_MOT);
                 localisation = TP3Utils.validerLngChaine(MSG_SOL_LOC, MSG_ERR_LOC, MIN_LOC, MAX_LOC);
-                //enlever les espaces
+
                 ObjetPerdu objet1 = new ObjetPerdu(categorie, date1, localisation);
+            //enlever les espaces
 
                 objet1.motsCles = chaineMotsCles.split("\\s+");
                 for (int i = 0; i < objetsPerdus.length; i++) {
                     objet1.motsCles[i] = objet1.motsCles[i].trim();
                 }
+
                 System.out.println();
 
                 if (nbrElemObjet == objetsPerdus.length) {
@@ -132,7 +141,7 @@ public class GestionObjetsPerdus {
                 System.out.println(ENTETE_CHOIX2);
 
                 numeroObjetPerdu = TP3Utils.validerTypeEntierStrictPositif(MSG_SOL_OBJETPERDU, MSG_ERREUR);
-                if (objetsPerdus[i] = null) { //si L'objet n'est pas dans le tableau
+                if (objetsPerdus == null) { //si L'objet n'est pas dans le tableau
                     // valider le choix
                     System.out.println("** LA REMISE DE L'OBJET PERDU A ETE EFFECTUEE AVEC SUCCES **");
                     taperEntree();
@@ -155,9 +164,82 @@ public class GestionObjetsPerdus {
                 }
 
             } else if (choix == 3) {
+                String chaine;
+                System.out.println(ENTETE_CHOIX3);
+                chaine = TP3Utils.validerLngChaine(MSG_SOL_MOT,MSG_ERR_MOT,MIN_MOT,MAX_MOT);
+                if (objetsPerdus != null) { //objet consigne existe dans le tableau
 
+                } else {
+                    System.out.println("RESULTAT(S) DE LA RECHERCHE :");
+                    System.out.println("** AUCUN OBJET TROUVE **");
+                    taperEntree();
+                }
+
+            } else if (choix == 4) {
+                System.out.println(ENTETE_CHOIX4);
+
+
+                if(objetsPerdus== null){    //condition a changer
+                    System.out.println("** AUCUN OBJET PERDU N'EST CONSIGNE **\n");
+                    taperEntree();
+                } else {
+                    rep = TP3Utils.validerCaractere(MSG_SOL_RES,MSG_ERR_RES,CAR_MIN,CAR_MAX);
+                    if (rep == 'd') {
+                        System.out.println("Date :");
+                        jour = TP3Utils.validerTypeEntierStrictPositif(MSG_SOL_JOUR, MSG_ERREUR_CHOIX);
+                        mois = TP3Utils.validerTypeEntierStrictPositif(MSG_SOL_MOIS, MSG_ERREUR_CHOIX);
+                        annee = TP3Utils.validerTypeEntierStrictPositif(MSG_SOL_ANNEE, MSG_ERREUR_CHOIX);
+                        System.out.println("RESULTAT(S) DE LA RECHERCHE :");
+                        //AFFICHER LES OBJETS PAR dATE
+                        System.out.println("** AUCUN OBJET TROUVE **");
+                        taperEntree();
+
+                    } else if ( rep == 'p') {
+                        System.out.println("Date du debut de la periode : ");
+                        jour = TP3Utils.validerTypeEntierStrictPositif(MSG_SOL_JOUR, MSG_ERREUR_CHOIX);
+                        mois = TP3Utils.validerTypeEntierStrictPositif(MSG_SOL_MOIS, MSG_ERREUR_CHOIX);
+                        annee = TP3Utils.validerTypeEntierStrictPositif(MSG_SOL_ANNEE, MSG_ERREUR_CHOIX);
+                        System.out.println("Date de la fin de la periode :");
+                        jour = TP3Utils.validerTypeEntierStrictPositif(MSG_SOL_JOUR, MSG_ERREUR_CHOIX);
+                        mois = TP3Utils.validerTypeEntierStrictPositif(MSG_SOL_MOIS, MSG_ERREUR_CHOIX);
+                        annee = TP3Utils.validerTypeEntierStrictPositif(MSG_SOL_ANNEE, MSG_ERREUR_CHOIX);
+                        System.out.println("RESULTAT(S) DE LA RECHERCHE :");
+                                                                                            //AFFICHER LES OBJETS PAR periode
+                        taperEntree();
+
+                    }
+                }
+
+            } else if (choix == 5) {
+                System.out.println(ENTETE_CHOIX5);
+
+
+                if (objetsPerdus==null) {  //condition a changer
+                    System.out.println("");
+                    System.out.println("** AUCUN OBJET PERDU N'EST CONSIGNE **");
+                    taperEntree();
+                } else {
+                    System.out.println(MENU_CATEGORIES);
+                    categorie = TP3Utils.validerEntier(MSG_SOL_CATEGORIES, MSG_ERREUR_CATEGORIES, 1, 5);
+                    System.out.println("RESULTAT(S) DE LA RECHERCHE :");
+                    taperEntree();
+
+                }
+
+            } else if (choix == 6) {
+                System.out.println(ENTETE_CHOIX6);
+
+                if (objetsPerdus==null) {
+                    System.out.println("");
+                    System.out.println("** AUCUN OBJET PERDU N'EST CONSIGNE **");
+                    taperEntree();
+                } else {
+                                        //AFFICHER DE TOUS LES OBJETS CONSIGNES
+                    taperEntree();
+
+                }
             }
-        }while(choix!=7);
+        } while(choix != 7);
         System.out.println("\n\nAUREVOIR !");
 
     }
